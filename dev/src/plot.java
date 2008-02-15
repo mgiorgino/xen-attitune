af82033a1ea68a9b8216f294dcd6ecde24192c69
MogShooter <MogShooter@b90cce90-7c44-0410-9426-7bfa32945d98>
2008-02-15 12:27:14 +0000

    Creation de l'arbo dev pour les fichiers de developpement Ajout des librairies

---
+++ dev/src/plot.java
@@ -0,0 +1,232 @@
+package graphique;
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation; either version 2.1 of the License, or 
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
+ * USA.  
+ *
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * in the United States and other countries.]
+ *
+ * -------------------------
+ * TimeSeriesChartDemo1.java
+ * -------------------------
+ * (C) Copyright 2003-2007, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   ;
+ *
+ * Changes
+ * -------
+ * 09-Mar-2005 : Version 1, copied from the demo collection that ships with
+ *               the JFreeChart Developer Guide (DG);
+ *
+ */
+
+
+import java.awt.Color;
+import java.awt.Frame;
+import java.util.Collection;
+import java.util.TreeMap;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.IntervalXYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RectangleInsets;
+
+/**
+ * An example of a time series chart.  For the most part, default settings are 
+ * used, except that the renderer is modified to show filled shapes (as well as 
+ * lines) at each data point.
+ */
+public class Histograme extends JFrame {
+	TreeMap<Integer, Integer> data;
+    /**
+     * A demonstration application showing how to create a simple time series 
+     * chart.  This example uses monthly data.
+     *
+     * @param title  the frame title.
+     */
+    public Histograme(String title, TreeMap<Integer, Integer> data) {
+        super(title);
+        this.data = data;
+        ChartPanel chartPanel = (ChartPanel) createDemoPanel();
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        chartPanel.setMouseZoomable(true, false);
+        setContentPane(chartPanel);
+		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
+
+    /**
+     * Creates a chart.
+     * 
+     * @param dataset  a dataset.
+     * 
+     * @return A chart.
+     */
+    private static JFreeChart createChart(IntervalXYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createXYBarChart(
+            "Resources' Histogram",  // title
+            "Step Number",             // x-axis label
+            false,				// x-axis label?
+            "Frequency",   // y-axis label
+            dataset,            // data
+            PlotOrientation.VERTICAL,		//Orientation
+            false,               // create legend?
+            true,               // generate tooltips?
+            false               // generate URLs?
+        );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+        
+        XYItemRenderer r = plot.getRenderer();
+        if (r instanceof XYLineAndShapeRenderer) {
+            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
+            renderer.setBaseShapesVisible(true);
+            renderer.setBaseShapesFilled(true);
+        }
+       return chart;
+
+    }
+    
+    /**
+     * Creates a dataset, consisting of two series of monthly data.
+     *
+     * @return The dataset.
+     */
+    private IntervalXYDataset createDataset() {
+    	
+        XYSeries s1 = new XYSeries("Resource ");
+        for (Integer i : data.keySet()){
+        	s1.add(i, data.get(i));
+        }
+        
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(s1);
+        
+        return dataset;
+
+    }
+
+    /**
+     * Creates a panel for the demo (used by SuperDemo.java).
+     * 
+     * @return A panel.
+     */
+    public JPanel createDemoPanel() {
+        JFreeChart chart = createChart(createDataset());
+        return new ChartPanel(chart);
+    }
+    
+    
+//    public static void main(String[] args) {
+//
+//        TimeSeries1 demo = new TimeSeries1(
+//                "Time Series Chart Demo 1");
+//        demo.pack();
+//        RefineryUtilities.centerFrameOnScreen(demo);
+//        demo.setVisible(true);
+//
+//    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
