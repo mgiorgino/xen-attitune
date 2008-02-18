b119bb86860dc8fad3985101ac415e8196de795d
lavachemauve <lavachemauve@b90cce90-7c44-0410-9426-7bfa32945d98>
2008-02-18 10:34:47 +0000

    plop

--- scripts/script_chrono2.sh
+++ scripts/script_chrono2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mig=0
+
 while :
 do
 	if [ $mig -eq 0 ];
@@ -8,14 +9,18 @@ do
         	then
 			mig=1;
 			echo debut;
-                	./chrono_launch;
+                	a=`./chrono2 | cut -d ' ' -f 2`;
         	fi
 	else
 		if xm list | grep $1 | grep -v p | grep -v migrating > /dev/null
         	then
 			mig=0;
 			echo fin;
-                	./chrono_launch;
+                	b=`./chrono2 | cut -d ' ' -f 2`;
+			val=$(( $b - $a ));
+			sec=$(( $val / 1000000 ));
+			usec=$(( $val - $sec ));
+			echo diff : $sec sec $usec usec;
         	fi
 	fi
 done
