a6015b4cb38ad4a5777facaaab087770cdd7a457
CLeJ37 <CLeJ37@b90cce90-7c44-0410-9426-7bfa32945d98>
2008-02-21 17:26:42 +0000

    RMI

---
+++ Tests/RMIChiant/src/MonObjet.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+
+
+public class MonObjet implements Serializable {
+	
+	private static final long serialVersionUID = -8716644838547617037L;
+	
+	public int i;
+	
+	public MonObjet(int j){
+		i=j;
+	}
+	
+}
