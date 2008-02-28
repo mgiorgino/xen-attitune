8e5b22971d686e301298c4dfd8db9b6e0864cebb
Clabird.Doudou <Clabird.Doudou@b90cce90-7c44-0410-9426-7bfa32945d98>
2008-02-28 08:05:23 +0000

    Ajout des fichiers du serveur

--- trunk/ConfigurationFiles/network/static147.sh
+++ trunk/ConfigurationFiles/network/static147.sh
@@ -1,5 +1,5 @@
-cp ~adminxen/network/interfaces_files/interfaces_static_147 /etc/network/interfaces
+cp ./interfaces_files/interfaces_static_147 /etc/network/interfaces
 /etc/init.d/networking restart
 
-cp ~adminxen/network/dhcpdconf_files/dhcpd147.conf /etc/dhcp3/dhcpd.conf
+cp ./dhcpdconf_files/dhcpd147.conf /etc/dhcp3/dhcpd.conf
 /etc/init.d/dhcp3-server restart
--- trunk/ConfigurationFiles/network/static192.sh
+++ trunk/ConfigurationFiles/network/static192.sh
@@ -1,5 +1,5 @@
-cp ~adminxen/network/interfaces_files/interfaces_static_192 /etc/network/interfaces
+cp ./interfaces_files/interfaces_static_192 /etc/network/interfaces
 /etc/init.d/networking restart
 
-cp ~adminxen/network/dhcpdconf_files/dhcpd192.conf /etc/dhcp3/dhcpd.conf
+cp ./dhcpdconf_files/dhcpd192.conf /etc/dhcp3/dhcpd.conf
 /etc/init.d/dhcp3-server restart
