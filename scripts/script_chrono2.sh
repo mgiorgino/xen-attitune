8f125574f6f60e5bb3a635e8fe57669473438440
CLeJ37 <CLeJ37@b90cce90-7c44-0410-9426-7bfa32945d98>
2008-02-28 10:00:58 +0000

    A little commit with RMI Tests.

--- scripts/script_chrono2.sh
+++ scripts/script_chrono2.sh
@@ -6,17 +6,17 @@ do
 	if [ $mig -eq 0 ];
 	then	
 		if xm list | grep $1 | grep migrating > /dev/null
-        	then
+		then
 			mig=1;
 			echo debut;
-                	a=`./chrono2 | cut -d ' ' -f 2`;
-        	fi
+			a=`./chrono2 | cut -d ' ' -f 2`;
+   	fi
 	else
 		if xm list | grep $1 | grep -v p | grep -v migrating > /dev/null
         	then
 			mig=0;
 			echo fin;
-                	b=`./chrono2 | cut -d ' ' -f 2`;
+			b=`./chrono2 | cut -d ' ' -f 2`;
 			val=$(( $b - $a ));
 			sec=$(( $val / 1000000 ));
 			usec=$(( $val - $sec ));
