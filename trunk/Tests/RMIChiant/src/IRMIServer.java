51bfd21ac4a447d584ee3348a2c13c03f29ee0be
CLeJ37 <CLeJ37@b90cce90-7c44-0410-9426-7bfa32945d98>
2008-02-29 08:10:41 +0000

    On enlève le RMIChiant qui plait pas à zek et on rajoute le ptit ping

--- trunk/Tests/RMIChiant/src/IRMIServer.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-
-public interface IRMIServer extends Remote {
-
-	public SizeableObject pingPong(SizeableObject o) throws RemoteException;
-
-}
